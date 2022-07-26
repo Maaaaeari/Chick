b0d4b32beb987d952e71e6bf50de10faf1b3b47d
ililililililililililililililil <87892213+ililililililililililililililil@users.noreply.github.com>
2022-07-26 22:24:32 +0900

    약 알람 수정

---
+++ app/src/main/java/com/example/chick/EditAlarmActivity.kt
@@ -0,0 +1,20 @@
+package com.example.chick
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+
+class EditAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_alarm)
+        
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_edit_alarm, menu)
+        return true
+    }
+}
