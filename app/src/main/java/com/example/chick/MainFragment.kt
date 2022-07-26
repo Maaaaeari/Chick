3743ab77a19821a9a1039d411dc395e2bf71225b
serak0310 <serak0310@naver.com>
2022-07-26 23:51:45 +0900

    수정ing

--- app/src/main/java/com/example/chick/MainFragment.kt
+++ app/src/main/java/com/example/chick/MainFragment.kt
@@ -1,19 +1,34 @@
 package com.example.chick
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class MainFragment : Fragment() {
+
+    lateinit var btnPlus : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_main, container, false)
+        btnPlus = view.findViewById(R.id.btnDelete)
+
+        btnPlus.setOnClickListener{
+            //val intent = Intent(activity, SharePostActivity::class.java)
+            //startActivity(intent)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        //return inflater.inflate(R.layout.fragment_main, container, false)
+
+        return view
     }
 
 
