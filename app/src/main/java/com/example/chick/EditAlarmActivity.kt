cd1a33e368d67c349ed264b546af6ca227ce60d0
ililililililililililililililil <87892213+ililililililililililililililil@users.noreply.github.com>
2022-07-27 04:24:44 +0900

    약 알람 수정, 알람화면

--- app/src/main/java/com/example/chick/EditAlarmActivity.kt
+++ app/src/main/java/com/example/chick/EditAlarmActivity.kt
@@ -2,22 +2,13 @@ package com.example.chick
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 class EditAlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_alarm)
-        
-        //supportActionBar?.setDisplayShowTitleEnabled(false)
-        //supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
-
-    /*
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_edit_alarm, menu)
-        return true
-    }
-
-     */
 }
---
+++ app/src/main/java/com/example/chick/MedAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_med_alarm)
+    }
+}
