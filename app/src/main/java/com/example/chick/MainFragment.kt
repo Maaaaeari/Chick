f74fe71870349447c9971a71ad33246caab5f5fa
serak0310 <serak0310@naver.com>
2022-07-27 18:14:31 +0900

    [add] db 테이블 더미데이터

--- app/src/main/java/com/example/chick/MainFragment.kt
+++ app/src/main/java/com/example/chick/MainFragment.kt
@@ -1,12 +1,14 @@
 package com.example.chick
 
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -14,12 +16,16 @@ class MainFragment : Fragment() {
 
     lateinit var btnPlus : FloatingActionButton
 
+    lateinit var dbManager: DBManager
+    lateinit var sqlDB: SQLiteDatabase
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_main, container, false)
 
+        dbManager = DBManager(context, "drug", null, 1)
 
 
         btnPlus = view.findViewById(R.id.btnMainAdd)
@@ -27,6 +33,11 @@ class MainFragment : Fragment() {
         btnPlus.setOnClickListener{
             val intent = Intent(activity, EditAlarmActivity::class.java)
             startActivity(intent)
+
+//            sqlDB = dbManager.writableDatabase
+//            sqlDB.execSQL("INSERT INTO drugDB VALUES (1, 'vitamin', 'am', 2, 30, 'Mon', 2, 30, 0, 1);")
+//            sqlDB.close()
+            //Toast.makeText(applicationContext, "입력됨", Toast.LENGTH_SHORT).show()
         }
 
         // Inflate the layout for this fragment
