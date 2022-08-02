8a83f33e8ebbd9291d76bcf24bbd3a0d31c41b42
serak0310 <serak0310@naver.com>
2022-08-02 11:17:58 +0900

    [add] 전체복용(currentNumber, eatDone, goalDone 업데이트)

--- app/src/main/java/com/example/chick/MedAlarmActivity.kt
+++ app/src/main/java/com/example/chick/MedAlarmActivity.kt
@@ -6,16 +6,17 @@ import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MedAlarmActivity : AppCompatActivity() {
 
-    lateinit var txtMedDrugName: TextView
+    lateinit var txtMedDrugName1: TextView
+    lateinit var txtMedDrugName2: TextView
     lateinit var medAlarmCancel: Button
     lateinit var medAlarmConfirm: Button
 
@@ -29,16 +30,35 @@ class MedAlarmActivity : AppCompatActivity() {
         setContentView(R.layout.activity_med_alarm)
 
         // 바인딩
-        txtMedDrugName = findViewById(R.id.txtMedDrugName)
+        txtMedDrugName1 = findViewById(R.id.txtMedDrugName1)
+        txtMedDrugName2 = findViewById(R.id.txtMedDrugName2)
         medAlarmCancel = findViewById(R.id.medAlarmCancel)
         medAlarmConfirm = findViewById(R.id.medAlarmConfirm)
 
         // DB 생성
         dbManager = DBManager(this, "drugDB", null, 1)
 
+        // 현재 요일 구하기
+        val long_now = System.currentTimeMillis()
+        val t_date = Date(long_now)
+        val tDaysOfWeek_dateFormat = SimpleDateFormat("E", Locale("ko", "KR"))
+        val tDaysOfWeek = tDaysOfWeek_dateFormat.format(t_date)     // 요일
+
         // 오늘 약 리스트 가져오기
-        alarmTodayName = selectTodayDrug()
-        txtMedDrugName.text = alarmTodayName
+        alarmTodayName = selectTodayDrug(tDaysOfWeek)
+        if(alarmTodayName != ""){
+            // 오늘 먹을 약이 있다면
+            txtMedDrugName1.text = alarmTodayName
+            txtMedDrugName2.text = "모두 복용하셨나요?"
+            medAlarmCancel.setText("나중에 먹기")
+            medAlarmConfirm.setVisibility(View.VISIBLE)
+        }else{
+            // 오늘 먹을 약이 없다면
+            txtMedDrugName1.text = ""
+            txtMedDrugName2.text = "오늘 복용할 약이 없었군요!"
+            medAlarmCancel.setText("확인")
+            medAlarmConfirm.setVisibility(View.GONE)
+        }
 
         // 나중에 먹기 버튼
         medAlarmCancel.setOnClickListener {
@@ -49,19 +69,18 @@ class MedAlarmActivity : AppCompatActivity() {
 
         // 복용 완료 버튼
         medAlarmConfirm.setOnClickListener {
-
+            // 현재 요일 약 모두 복용완료
+            eatTodayDrugAll(tDaysOfWeek)
+            // 약 알람 리스트로 이동
+            val intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
         }
 
     }
 
     // 현재 요일의 복용약 이름 조회
     @SuppressLint("Range")
-    private fun selectTodayDrug(): String{
-        // 현재 요일 구하기
-        val long_now = System.currentTimeMillis()
-        val t_date = Date(long_now)
-        val tDaysOfWeek_dateFormat = SimpleDateFormat("E", Locale("ko", "KR"))
-        val tDaysOfWeek = tDaysOfWeek_dateFormat.format(t_date)     // 요일
+    private fun selectTodayDrug(tDaysOfWeek: String): String{
 
         // 현재 요일의 알람 조회
         val selectTodayDrug = "select * from drugTBL where goalDone=0 AND daysOfWeek LIKE '%${tDaysOfWeek}%';"
@@ -81,25 +100,29 @@ class MedAlarmActivity : AppCompatActivity() {
             input.add(medName)
         }
 
-//        if(cursor != null){
-//            if (cursor.moveToFirst()){
-//                do {
-//                    var medName = cursor.getString(cursor.getColumnIndex("medName")).toString()
-//                    input.add(medName)
-//                } while (cursor.moveToNext())
-//            }
-//            cursor.close()
-//            alarmTodayName =""
-//        }
-
         // 오늘 먹어야 할 약 이름
         alarmTodayName = java.lang.String.join(separator, input)
-
-        Log.d("abcdef", alarmTodayName)
 
         cursor.close()
         sqlDB.close()
 
         return alarmTodayName
+    }
+
+    // 현재 요일 약 모두 복용완료
+    private fun eatTodayDrugAll(tDaysOfWeek: String){
+
+        // 현재 요일의 약 복용완료 업데이트
+        val eatDoneTodayUpdate = "update drugTBL set eatDone=1, currentNumber=(currentNumber+eatNumber) where daysOfWeek LIKE '%${tDaysOfWeek}%' AND eatDone=0 AND totalNumber>(currentNumber+eatNumber);"
+        val goalDoneTodayUpdate = "update drugTBL set eatDone=1, currentNumber=(currentNumber+eatNumber), goalDone=1 where daysOfWeek LIKE '%${tDaysOfWeek}%' AND eatDone=0 AND totalNumber<=(currentNumber+eatNumber);"
+
+        // 쓰기전용 데이터베이스 변수
+        sqlDB = dbManager.writableDatabase
+
+        sqlDB.execSQL(eatDoneTodayUpdate)
+        sqlDB.execSQL(goalDoneTodayUpdate)
+
+        sqlDB.close()
+        dbManager.close()
     }
 }
